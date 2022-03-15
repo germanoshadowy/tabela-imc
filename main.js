0cbeda36d765af9382c8ee95c62422f2cada4ae3
Germano Pinheiro <76917944+germanoshadowy@users.noreply.github.com>
2022-03-14 21:36:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,67 @@
-let nome = document.getElementById("nome").value;
-let altura = document.getElementById("altura").value;
-let peso = document.getElementById("peso").value;
+let nome = document.getElementById("nome");
+let altura = document.getElementById("altura");
+let peso = document.getElementById("peso");
+// var altura = "";
+// altura = parseFloat(document.getElementById("altura").value);
+// var peso = "";
+// peso = parseFloat(document.getElementById("peso").value);
 let registrar = document.getElementById("btn-reg");
-let imc = altura*altura/peso;
 let body = document.querySelector("body");
 let div = document.createElement("div");
 let table = document.createElement("table");
 let thead = document.createElement("thead");
 let tbody = document.createElement("tbody");
+let botao = document.getElementById("btn-reg");
+let imc = peso/(altura*altura);
+botao.addEventListener("click", calculo);
+
+// function calculo(){
+
+//   console.log(imc);
+//   let linha = document.createElement("tr");
+//   let tNome = document.createElement("th");
+//   let tPeso = document.createElement("th");
+//   let tAltura = document.createElement("th");
+//   let tIMC = document.createElement("th");
+//   tNome.innerHTML = nome.value;
+//   tPeso.innerHTML = peso;
+//   tAltura.innerHTML = altura;
+//   tIMC.innerHTML = imc.value;
+
+//   linha.appendChild(tNome);
+//   linha.appendChild(tAltura);
+//   linha.appendChild(tPeso);
+//   linha.appendChild(tIMC);
+//   thead.appendChild(linha);
+
+//   table.appendChild(thead);
+//   table.appendChild(tbody);
+// }
 
 function calculo()
 {
-    
-let linha = document.createElement("tr");
-let tNome = document.createElement("th");
-let tPeso = document.createElement("th");
-let tAltura = document.createElement("th");
-let tIMC = document.createElement("th");
-tNome.innerHTML = nome.innerHTML;
-tPeso.innerHTML = "PESO";
-tAltura.innerHTML = "ALTURA";
+let linha1 = document.createElement("tr");
+let tNome = document.createElement("td");
+let tPeso = document.createElement("td");
+let tAltura = document.createElement("td");
+let tIMC = document.createElement("td");
+
+tNome.innerHTML = nome.value;
+tPeso.innerHTML = peso.value;
+tAltura.innerHTML = altura.value;
 tIMC.innerHTML = "IMC";
 
-linha.appendChild(tNome);
-linha.appendChild(tPeso);
-linha.appendChild(tAltura);
-linha.appendChild(tIMC);
-thead.appendChild(linha);
+linha1.appendChild(tNome);
+linha1.appendChild(tPeso);
+linha1.appendChild(tAltura);
+linha1.appendChild(tIMC);
+thead.appendChild(linha1);
 
 table.appendChild(thead);
 table.appendChild(tbody);
 }
 
-// function calculo()
-// {
-// let linha1 = document.createElement("tr");
-// let tNome = document.createElement("th");
-// let tPeso = document.createElement("th");
-// let tAltura = document.createElement("th");
-// let tIMC = document.createElement("th");
 
-// tNome.innerHTML = "NOME";
-// tPeso.innerHTML = "PESO";
-// tAltura.innerHTML = "ALTURA";
-// tIMC.innerHTML = "IMC";
-
-// linha1.appendChild(tNome);
-// linha1.appendChild(tPeso);
-// linha1.appendChild(tAltura);
-// linha1.appendChild(tIMC);
-// thead.appendChild(linha1);
-
-// table.appendChild(thead);
-// table.appendChild(tbody);
-// }
-
-let botao = document.getElementById("btn-reg");
-botao.addEventListener("click", calculo);
 
 document.querySelector("body").appendChild(table);
 
